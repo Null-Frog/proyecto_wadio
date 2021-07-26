c01b912b1779bbe99ad406a6193056a152d22798
rubentxogg <rubentxogg@hotmail.com>
2021-07-26 12:47:09 +0200

    [ADD] Backend y Frontend conectados

--- src/index.js
+++ src/index.js
@@ -27,6 +27,9 @@ const createWindow = () => {
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
   
+  mainWindow.webContents.on('did-finish-load', function(){
+    mainWindow.webContents.send('message', "Back y front connectados");
+  });
 };
 
 // This method will be called when Electron has finished
@@ -53,3 +56,113 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+const qrcode = require('qrcode-terminal');
+require('dotenv').config();
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+  console.log('[!] Client is ready!');
+});
+
+client.initialize();
+
+// Speech to text
+const fs = require('fs');
+
+const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const speechToText = new SpeechToTextV1({
+  authenticator: new IamAuthenticator({
+    apikey: process.env.APIKEY
+  }),
+  serviceUrl: process.env.URL
+});
+
+const params = {
+  objectMode: true,
+  contentType: 'audio/ogg;codecs=opus',
+  model: 'es-ES_NarrowbandModel',
+  wordConficence: true
+};
+
+const recognizeStream = speechToText.recognizeUsingWebSocket(params);
+
+recognizeStream.on('data', function (event) { onEvent('Data:', event); });
+recognizeStream.on('error', function (event) { onEvent('Error:', event); });
+recognizeStream.on('close', function (event) { onEvent('Close:', event); });
+
+// Display events on the console.
+function onEvent(name, event) {
+  console.log(name, JSON.stringify(event, null, 2));
+};
+
+function resolveAfter3Seconds(x) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(x);
+    }, 3000);
+  });
+}
+
+/**
+ * Cada vez que el cliente reciba un archivo de tipo audio, se le contestará con un mensaje
+ */
+client.on('message', async msg => {
+  let chat = await msg.getChat();
+
+  if (msg.hasMedia && !chat.isGroup) {
+    const media = await msg.downloadMedia();
+
+    if (media.mimetype == "audio/ogg; codecs=opus") {
+      msg.reply('No me mandéis audios, por favor');
+
+      /**
+       * Transforma bin a audio
+       */
+      const buff = Buffer.from(media.data, 'base64');
+      fs.writeFileSync('audio/audioGenerado.ogg', buff);
+      console.log("[!] Audio generado");
+
+      /**
+       * Lee el archivo de audio
+       */
+      async function leerAudio() {
+        const leerAudio = fs.createReadStream(process.env.AUDIO);
+
+        const recognizeParams = {
+          audio: leerAudio,
+          contentType: 'audio/ogg;codecs=opus',
+          model: 'es-ES_NarrowbandModel',
+          endOfPhraseSilenceTime: 120.0
+        };
+
+        var audioObject;
+        var mensajeAudio;
+        var fiabilidad;
+
+        const transcribirAudio = speechToText.recognize(recognizeParams)
+        .then(SpeechRecognitionResults => {
+          audioObject = SpeechRecognitionResults;
+        })
+        .catch(err =>{
+          console.log('error', err);
+        });
+
+        await resolveAfter3Seconds(transcribirAudio);
+
+        mensajeAudio = audioObject.result.results[0].alternatives[0].transcript;
+        fiabilidad = audioObject.result.results[0].alternatives[0].confidence;
+        console.log("[!] Audio leído");
+        msg.reply("[Transcripción de audio]\n\n"+mensajeAudio+"\n\n[Fiabilidad: "+fiabilidad+"]");
+      }
+
+      leerAudio();
+    }
+  }
+});
