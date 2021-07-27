79dccc436c7b1459cf03f80c715565365a89158d
rubentxogg <rubentxogg@hotmail.com>
2021-07-27 17:18:15 +0200

    [ADD] Falta pasar el códgio

--- src/index.js
+++ src/index.js
@@ -26,15 +26,21 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
-  
-  
-    const ipcMain = require('electron').ipcMain;
 
-    ipcMain.on('synMessage', (event, args) => {
-      event.returnValue = args;
-      userMessage = args;
-      mainWindow.webContents.send('message', userMessage);
-    });
+
+  const ipcMain = require('electron').ipcMain;
+
+  ipcMain.on('synMessage', (event, args) => {
+    event.returnValue = args;
+    userMessage = args;
+    mainWindow.webContents.send('message', userMessage);
+  });
+
+  ipcMain.on('synActivar', (event, args) => {
+    event.returnValue = args;
+    activarCliente();
+    mainWindow.webContents.send('elQR', "Generado");
+  });
 
 };
 var userMessage;
@@ -63,113 +69,115 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
-const qrcode = require('qrcode-terminal');
-require('dotenv').config();
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+function activarCliente() {
+  const qrcode = require('qrcode-terminal');
+  require('dotenv').config();
+  const { Client } = require('whatsapp-web.js');
+  const client = new Client();
 
-client.on('qr', qr => {
-  qrcode.generate(qr, { small: true });
-});
+  client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+  });
 
-client.on('ready', () => {
-  console.log('[!] Client is ready!');
-});
+  client.on('ready', () => {
+    console.log('[!] Client is ready!');
+  });
 
-client.initialize();
+  client.initialize();
 
-// Speech to text
-const fs = require('fs');
+  // Speech to text
+  const fs = require('fs');
 
-const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
-const { IamAuthenticator } = require('ibm-watson/auth');
+  const SpeechToTextV1 = require('ibm-watson/speech-to-text/v1');
+  const { IamAuthenticator } = require('ibm-watson/auth');
 
-const speechToText = new SpeechToTextV1({
-  authenticator: new IamAuthenticator({
-    apikey: process.env.APIKEY
-  }),
-  serviceUrl: process.env.URL
-});
+  const speechToText = new SpeechToTextV1({
+    authenticator: new IamAuthenticator({
+      apikey: process.env.APIKEY
+    }),
+    serviceUrl: process.env.URL
+  });
 
-const params = {
-  objectMode: true,
-  contentType: 'audio/ogg;codecs=opus',
-  model: 'es-ES_NarrowbandModel',
-  wordConficence: true
-};
+  const params = {
+    objectMode: true,
+    contentType: 'audio/ogg;codecs=opus',
+    model: 'es-ES_NarrowbandModel',
+    wordConficence: true
+  };
 
-const recognizeStream = speechToText.recognizeUsingWebSocket(params);
+  const recognizeStream = speechToText.recognizeUsingWebSocket(params);
 
-recognizeStream.on('data', function (event) { onEvent('Data:', event); });
-recognizeStream.on('error', function (event) { onEvent('Error:', event); });
-recognizeStream.on('close', function (event) { onEvent('Close:', event); });
+  recognizeStream.on('data', function (event) { onEvent('Data:', event); });
+  recognizeStream.on('error', function (event) { onEvent('Error:', event); });
+  recognizeStream.on('close', function (event) { onEvent('Close:', event); });
 
-// Display events on the console.
-function onEvent(name, event) {
-  console.log(name, JSON.stringify(event, null, 2));
-};
+  // Display events on the console.
+  function onEvent(name, event) {
+    console.log(name, JSON.stringify(event, null, 2));
+  };
 
-function resolveAfter3Seconds(x) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(x);
-    }, 3000);
+  function resolveAfter3Seconds(x) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(x);
+      }, 3000);
+    });
+  }
+
+  /**
+   * Cada vez que el cliente reciba un archivo de tipo audio, se le contestará con un mensaje
+   */
+  client.on('message', async msg => {
+    let chat = await msg.getChat();
+
+    if (msg.hasMedia && !chat.isGroup) {
+      const media = await msg.downloadMedia();
+
+      if (media.mimetype == "audio/ogg; codecs=opus") {
+        msg.reply(userMessage);
+
+        /**
+         * Transforma bin a audio
+         */
+        const buff = Buffer.from(media.data, 'base64');
+        fs.writeFileSync('audio/audioGenerado.ogg', buff);
+        console.log("[!] Audio generado");
+
+        /**
+         * Lee el archivo de audio
+         */
+        async function leerAudio() {
+          const leerAudio = fs.createReadStream(process.env.AUDIO);
+
+          const recognizeParams = {
+            audio: leerAudio,
+            contentType: 'audio/ogg;codecs=opus',
+            model: 'es-ES_NarrowbandModel',
+            endOfPhraseSilenceTime: 120.0
+          };
+
+          var audioObject;
+          var mensajeAudio;
+          var fiabilidad;
+
+          const transcribirAudio = speechToText.recognize(recognizeParams)
+            .then(SpeechRecognitionResults => {
+              audioObject = SpeechRecognitionResults;
+            })
+            .catch(err => {
+              console.log('error', err);
+            });
+
+          await resolveAfter3Seconds(transcribirAudio);
+
+          mensajeAudio = audioObject.result.results[0].alternatives[0].transcript;
+          fiabilidad = audioObject.result.results[0].alternatives[0].confidence;
+          console.log("[!] Audio leído");
+          msg.reply("[Transcripción de audio]\n\n" + mensajeAudio + "\n\n[Fiabilidad: " + fiabilidad + "]");
+        }
+
+        leerAudio();
+      }
+    }
   });
 }
-
-/**
- * Cada vez que el cliente reciba un archivo de tipo audio, se le contestará con un mensaje
- */
-client.on('message', async msg => {
-  let chat = await msg.getChat();
-
-  if (msg.hasMedia && !chat.isGroup) {
-    const media = await msg.downloadMedia();
-
-    if (media.mimetype == "audio/ogg; codecs=opus") {
-      msg.reply(userMessage);
-
-      /**
-       * Transforma bin a audio
-       */
-      const buff = Buffer.from(media.data, 'base64');
-      fs.writeFileSync('audio/audioGenerado.ogg', buff);
-      console.log("[!] Audio generado");
-
-      /**
-       * Lee el archivo de audio
-       */
-      async function leerAudio() {
-        const leerAudio = fs.createReadStream(process.env.AUDIO);
-
-        const recognizeParams = {
-          audio: leerAudio,
-          contentType: 'audio/ogg;codecs=opus',
-          model: 'es-ES_NarrowbandModel',
-          endOfPhraseSilenceTime: 120.0
-        };
-
-        var audioObject;
-        var mensajeAudio;
-        var fiabilidad;
-
-        const transcribirAudio = speechToText.recognize(recognizeParams)
-        .then(SpeechRecognitionResults => {
-          audioObject = SpeechRecognitionResults;
-        })
-        .catch(err =>{
-          console.log('error', err);
-        });
-
-        await resolveAfter3Seconds(transcribirAudio);
-
-        mensajeAudio = audioObject.result.results[0].alternatives[0].transcript;
-        fiabilidad = audioObject.result.results[0].alternatives[0].confidence;
-        console.log("[!] Audio leído");
-        msg.reply("[Transcripción de audio]\n\n"+mensajeAudio+"\n\n[Fiabilidad: "+fiabilidad+"]");
-      }
-
-      leerAudio();
-    }
-  }
-});
